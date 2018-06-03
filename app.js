0abcec762f684d6a13e02e297c8a0b1601c247ad
Mateusz Bielak <mateusz.bielak@skygate.io>
2018-06-03 14:52:19 +0200

    Use yargs for arguments handling

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
 
-const command = process.argv[2];
+const argv = yargs.argv;
+const command = argv._[0];
 console.log(`Command: ${command}`);
+console.log('Yarg', argv);
 
 if (command === 'add') {
-    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('Listing all notes');
+    notes.getAll();
 } else if (command === 'read') {
-    console.log('Reading note');
+    notes.readNote(argv.title);
 } else if (command === 'remove') {
-    console.log('Removing note');
+    notes.removeNote(argv.title);
 } else {
     console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
-console.log('Starting notes.js')
+console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote')
-    return 'New note'
-}
+const addNote = (title, body) => {
+    console.log('Adding note', title, body);
+};
+
+const getAll = () => {
+    console.log('Getting all notes');
+};
+
+const readNote = title => {
+    console.log('Reading note', title);
+};
+
+const removeNote = title => {
+    console.log('Removing note', title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    readNote,
+    removeNote,
+};
